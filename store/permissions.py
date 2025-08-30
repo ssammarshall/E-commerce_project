f7ed6896642ce16909b82c1243166e07ffd32ff5
ssammarshall <ssammarshall@gmail.com>
2025-08-29 21:20:47 -0700

    Add permission IsAdminOrNotAuthenticated

--- store/permissions.py
+++ store/permissions.py
@@ -1,5 +1,11 @@
 from rest_framework import permissions
 
+class IsAdminOrNotAuthenticated(permissions.BasePermission):
+    def has_permission(self, request, view):
+        is_admin = bool(request.user and request.user.is_staff)
+        is_not_authenticated = bool(not request.user or not request.user.is_authenticated)
+        return bool(is_admin or is_not_authenticated)
+
 class IsAdminOrReadyOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
